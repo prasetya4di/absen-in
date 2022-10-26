4c50456696df76ac9e94f71d3c9f22118be54c90
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-26 21:45:08 +0700

    Add on click function to all button on main activity

--- app/src/main/java/com/pras/absenin/view/main/MainActivity.java
+++ app/src/main/java/com/pras/absenin/view/main/MainActivity.java
@@ -1,11 +1,14 @@
 package com.pras.absenin.view.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.pras.absenin.databinding.ActivityMainBinding;
+import com.pras.absenin.view.absen.AbsenActivity;
+import com.pras.absenin.view.history.HistoryActivity;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -16,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.btnAbsen.setOnClickListener(v -> startActivity(new Intent(this, AbsenActivity.class)));
+
+        binding.btnHistory.setOnClickListener(v -> startActivity(new Intent(this, HistoryActivity.class)));
+
+        binding.btnExit.setOnClickListener(v -> finishAndRemoveTask());
     }
 }

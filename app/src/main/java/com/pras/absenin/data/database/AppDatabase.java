893170bc9973ba36081722f1402d865b0fd236c5
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-26 22:58:59 +0700

    Add type converters

--- app/src/main/java/com/pras/absenin/data/database/AppDatabase.java
+++ app/src/main/java/com/pras/absenin/data/database/AppDatabase.java
@@ -2,11 +2,15 @@ package com.pras.absenin.data.database;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import com.pras.absenin.data.entity.Absent;
 import com.pras.absenin.data.source.local.AbsentDao;
+import com.pras.absenin.util.converters.DateConverter;
+import com.pras.absenin.util.converters.LatLngConverter;
 
 @Database(entities = {Absent.class}, version = 1)
+@TypeConverters({DateConverter.class, LatLngConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AbsentDao absentDao();
 }
---
+++ app/src/main/java/com/pras/absenin/util/converters/DateConverter.java
@@ -0,0 +1,17 @@
+package com.pras.absenin.util.converters;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public class DateConverter {
+    @TypeConverter
+    public Date fromTimestamp(long value) {
+        return new Date(value);
+    }
+
+    @TypeConverter
+    public long dateToTimestamp(Date date) {
+        return date.getTime();
+    }
+}

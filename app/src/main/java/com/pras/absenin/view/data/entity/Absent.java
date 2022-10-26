53235b647c3353a2e0705819744aeb5e519d6bbf
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-26 21:02:18 +0700

    Add absent entity

---
+++ app/src/main/java/com/pras/absenin/view/data/entity/Absent.java
@@ -0,0 +1,26 @@
+package com.pras.absenin.view.data.entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Date;
+
+@Entity
+public class Absent {
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "event_title")
+    public String eventTitle;
+
+    @ColumnInfo(name = "event_description")
+    public String eventDescription;
+
+    @ColumnInfo(name = "event_date")
+    public Date eventDate;
+
+    public LatLng location;
+}

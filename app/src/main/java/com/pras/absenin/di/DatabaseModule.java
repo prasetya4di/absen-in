c611206a97ef0e00bb594a5fd8a606b6170540dc
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-26 22:23:16 +0700

    Modify database module to also install in activity component

--- app/src/main/java/com/pras/absenin/di/DatabaseModule.java
+++ app/src/main/java/com/pras/absenin/di/DatabaseModule.java
@@ -10,11 +10,11 @@ import com.pras.absenin.data.source.local.AbsentDao;
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityComponent;
 import dagger.hilt.android.qualifiers.ApplicationContext;
-import dagger.hilt.components.SingletonComponent;
 
 @Module
-@InstallIn(SingletonComponent.class)
+@InstallIn(ActivityComponent.class)
 public class DatabaseModule {
 
     @Provides

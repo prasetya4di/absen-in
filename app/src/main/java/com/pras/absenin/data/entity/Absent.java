0e114b5bdf23eccaf69b587191ef8fb82751c59f
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-27 20:26:32 +0700

    Add serializable to absent entity

--- app/src/main/java/com/pras/absenin/data/entity/Absent.java
+++ app/src/main/java/com/pras/absenin/data/entity/Absent.java
@@ -5,20 +5,25 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-public class Absent {
+public class Absent implements Serializable {
     @PrimaryKey
     public int uid;
 
+    @SerializedName("event_title")
     @ColumnInfo(name = "event_title")
     public String eventTitle;
 
+    @SerializedName("event_description")
     @ColumnInfo(name = "event_description")
     public String eventDescription;
 
+    @SerializedName("event_date")
     @ColumnInfo(name = "event_date")
     public Date eventDate;
 

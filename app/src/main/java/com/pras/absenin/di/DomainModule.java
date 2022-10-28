afb424315dc593115c50cbffa9b1d55fbaeda015
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-28 20:46:13 +0700

    Add get current location use case

--- app/src/main/java/com/pras/absenin/di/DomainModule.java
+++ app/src/main/java/com/pras/absenin/di/DomainModule.java
@@ -1,32 +1,28 @@
 package com.pras.absenin.di;
 
-import android.content.Context;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.pras.absenin.domain.DoAbsent;
 import com.pras.absenin.domain.GetAbsentHistoryById;
 import com.pras.absenin.domain.GetAllAbsentHistory;
+import com.pras.absenin.domain.GetCurrentLocation;
 import com.pras.absenin.domain.impl.DoAbsentImpl;
 import com.pras.absenin.domain.impl.GetAbsentHistoryByIdImpl;
 import com.pras.absenin.domain.impl.GetAllAbsentHistoryImpl;
+import com.pras.absenin.domain.impl.GetCurrentLocationImpl;
 import com.pras.absenin.repository.AbsentRepository;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ViewModelComponent;
-import dagger.hilt.android.qualifiers.ApplicationContext;
 
 @Module
 @InstallIn(ViewModelComponent.class)
 public class DomainModule {
 
     @Provides
-    public DoAbsent provideDoAbsent(
-            @ApplicationContext Context context,
-            AbsentRepository absentRepository,
-            FusedLocationProviderClient fusedLocationProviderClient) {
-        return new DoAbsentImpl(context, absentRepository, fusedLocationProviderClient);
+    public DoAbsent provideDoAbsent(AbsentRepository absentRepository) {
+        return new DoAbsentImpl(absentRepository);
     }
 
     @Provides
@@ -37,5 +33,10 @@ public class DomainModule {
     @Provides
     public GetAbsentHistoryById provideGetAbsentById(AbsentRepository absentRepository) {
         return new GetAbsentHistoryByIdImpl(absentRepository);
+    }
+
+    @Provides
+    public GetCurrentLocation provideGetCurrentLocation(FusedLocationProviderClient fusedLocationProviderClient) {
+        return new GetCurrentLocationImpl(fusedLocationProviderClient);
     }
 }
---
+++ app/src/main/java/com/pras/absenin/domain/GetCurrentLocation.java
@@ -0,0 +1,9 @@
+package com.pras.absenin.domain;
+
+import android.location.Location;
+
+import com.google.android.gms.tasks.Task;
+
+public interface GetCurrentLocation {
+    Task<Location> getCurrentLocation();
+}

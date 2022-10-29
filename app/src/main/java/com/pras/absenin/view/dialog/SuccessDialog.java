d5461b2fb0a287ee55aa31fb697a4524708e737c
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-29 22:44:01 +0700

    Add success dialog

---
+++ app/src/main/java/com/pras/absenin/view/dialog/SuccessDialog.java
@@ -0,0 +1,24 @@
+package com.pras.absenin.view.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+
+import com.pras.absenin.R;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class SuccessDialog {
+    private final Activity activity;
+
+    public SuccessDialog(Activity myactivity) {
+        activity = myactivity;
+    }
+
+    public void showSuccessDialog(String eventName) {
+        SweetAlertDialog loadingDialog = new SweetAlertDialog(activity, SweetAlertDialog.SUCCESS_TYPE);
+        loadingDialog.setTitleText(activity.getString(R.string.success_text, eventName));
+        loadingDialog.setCancelable(false);
+        loadingDialog.setConfirmButton(activity.getString(R.string.exit_text), Dialog::onBackPressed);
+        loadingDialog.show();
+    }
+}

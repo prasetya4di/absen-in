f86ec35a76d7aa2992b8402d39192a691b74f9f3
Pras <prasetyaarfanto@gmail.com>
2022-12-20 14:44:16 +0700

    Provide all usecase

--- app/src/main/java/id/ac/stiki/doleno/absenin/di/DomainModule.java
+++ app/src/main/java/id/ac/stiki/doleno/absenin/di/DomainModule.java
@@ -6,15 +6,22 @@ import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ViewModelComponent;
+import id.ac.stiki.doleno.absenin.domain.AddEvent;
 import id.ac.stiki.doleno.absenin.domain.DoAbsent;
 import id.ac.stiki.doleno.absenin.domain.GetAbsentHistoryById;
 import id.ac.stiki.doleno.absenin.domain.GetAllAbsentHistory;
+import id.ac.stiki.doleno.absenin.domain.GetAllEvent;
 import id.ac.stiki.doleno.absenin.domain.GetCurrentLocation;
+import id.ac.stiki.doleno.absenin.domain.GetEventById;
+import id.ac.stiki.doleno.absenin.domain.impl.AddEventImpl;
 import id.ac.stiki.doleno.absenin.domain.impl.DoAbsentImpl;
 import id.ac.stiki.doleno.absenin.domain.impl.GetAbsentHistoryByIdImpl;
 import id.ac.stiki.doleno.absenin.domain.impl.GetAllAbsentHistoryImpl;
+import id.ac.stiki.doleno.absenin.domain.impl.GetAllEventImpl;
 import id.ac.stiki.doleno.absenin.domain.impl.GetCurrentLocationImpl;
+import id.ac.stiki.doleno.absenin.domain.impl.GetEventByIdImpl;
 import id.ac.stiki.doleno.absenin.repository.AbsentRepository;
+import id.ac.stiki.doleno.absenin.repository.EventRepository;
 
 @Module
 @InstallIn(ViewModelComponent.class)
@@ -38,5 +45,20 @@ public class DomainModule {
     @Provides
     public GetCurrentLocation provideGetCurrentLocation(FusedLocationProviderClient fusedLocationProviderClient) {
         return new GetCurrentLocationImpl(fusedLocationProviderClient);
+    }
+
+    @Provides
+    public GetAllEvent provideGetAllEvent(EventRepository eventRepository) {
+        return new GetAllEventImpl(eventRepository);
+    }
+
+    @Provides
+    public GetEventById provideGetEventById(EventRepository eventRepository) {
+        return new GetEventByIdImpl(eventRepository);
+    }
+
+    @Provides
+    public AddEvent provideAddEvent(EventRepository eventRepository) {
+        return new AddEventImpl(eventRepository);
     }
 }

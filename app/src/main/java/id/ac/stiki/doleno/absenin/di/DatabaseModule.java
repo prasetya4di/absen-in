04c2deaeef3ae990780a2cbf1acaaaae7101508c
Pras <prasetyaarfanto@gmail.com>
2022-12-20 14:01:37 +0700

    Provide firestore and other dao

--- app/src/main/java/id/ac/stiki/doleno/absenin/di/DatabaseModule.java
+++ app/src/main/java/id/ac/stiki/doleno/absenin/di/DatabaseModule.java
@@ -4,6 +4,8 @@ import android.content.Context;
 
 import androidx.room.Room;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
@@ -11,6 +13,8 @@ import dagger.hilt.android.components.ViewModelComponent;
 import dagger.hilt.android.qualifiers.ApplicationContext;
 import id.ac.stiki.doleno.absenin.data.database.AppDatabase;
 import id.ac.stiki.doleno.absenin.data.source.local.AbsentDao;
+import id.ac.stiki.doleno.absenin.data.source.local.EventDao;
+import id.ac.stiki.doleno.absenin.data.source.local.UserDao;
 
 @Module
 @InstallIn(ViewModelComponent.class)
@@ -26,7 +30,22 @@ public class DatabaseModule {
     }
 
     @Provides
+    public static FirebaseFirestore provideFireStore() {
+        return FirebaseFirestore.getInstance();
+    }
+
+    @Provides
     public AbsentDao provideAbsentDao(AppDatabase appDatabase) {
         return appDatabase.absentDao();
+    }
+
+    @Provides
+    public EventDao provideEventDao(AppDatabase appDatabase) {
+        return appDatabase.eventDao();
+    }
+
+    @Provides
+    public UserDao provideUserDao(AppDatabase appDatabase) {
+        return appDatabase.userDao();
     }
 }

69ea29933325558192e813872da686d190d232c9
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-12-21 15:34:03 +0700

    Add map parser for absent and event entity

--- app/src/main/java/id/ac/stiki/doleno/absenin/data/entity/Event.java
+++ app/src/main/java/id/ac/stiki/doleno/absenin/data/entity/Event.java
@@ -13,6 +13,10 @@ import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import id.ac.stiki.doleno.absenin.data.database.table.Column;
 
 @Entity
 public class Event implements Serializable, Parcelable {
@@ -35,6 +39,9 @@ public class Event implements Serializable, Parcelable {
     @SerializedName("event_description")
     @ColumnInfo(name = "event_description")
     public String eventDescription;
+    @SerializedName("event_organizer")
+    @ColumnInfo(name = "event_organizer")
+    public String eventOrganizer;
     @SerializedName("event_date")
     @ColumnInfo(name = "event_date")
     public Date eventDate;
@@ -43,18 +50,29 @@ public class Event implements Serializable, Parcelable {
     public Event() {
     }
 
-    public Event(int uid, String eventTitle, String eventDescription, Date eventDate, LatLng location) {
+    public Event(int uid, String eventTitle, String eventDescription, String eventOrganizer, Date eventDate, LatLng location) {
         this.uid = uid;
         this.eventTitle = eventTitle;
         this.eventDescription = eventDescription;
+        this.eventOrganizer = eventOrganizer;
         this.eventDate = eventDate;
         this.location = location;
+    }
+
+    public Event(Map<String, Object> data) {
+        this.uid = (int) data.get(Column.Event.UID.getColumnName());
+        this.eventTitle = (String) data.get(Column.Event.TITLE.getColumnName());
+        this.eventDescription = (String) data.get(Column.Event.DESCRIPTION.getColumnName());
+        this.eventOrganizer = (String) data.get(Column.Event.ORGANIZER.getColumnName());
+        this.eventDate = (Date) data.get(Column.Event.DATE.getColumnName());
+        this.location = (LatLng) data.get(Column.Event.LOCATION.getColumnName());
     }
 
     protected Event(Parcel in) {
         uid = in.readInt();
         eventTitle = in.readString();
         eventDescription = in.readString();
+        eventOrganizer = in.readString();
         location = in.readParcelable(LatLng.class.getClassLoader());
     }
 
@@ -68,6 +86,22 @@ public class Event implements Serializable, Parcelable {
         dest.writeInt(uid);
         dest.writeString(eventTitle);
         dest.writeString(eventDescription);
+        dest.writeString(eventOrganizer);
         dest.writeParcelable(location, flags);
+    }
+
+    public String getDocumentPath() {
+        return "event-" + uid;
+    }
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> event = new HashMap<>();
+        event.put(Column.Event.UID.getColumnName(), uid);
+        event.put(Column.Event.TITLE.getColumnName(), eventTitle);
+        event.put(Column.Event.DESCRIPTION.getColumnName(), eventDescription);
+        event.put(Column.Event.ORGANIZER.getColumnName(), eventOrganizer);
+        event.put(Column.Event.DATE.getColumnName(), eventDate);
+        event.put(Column.Event.LOCATION.getColumnName(), location);
+        return event;
     }
 }

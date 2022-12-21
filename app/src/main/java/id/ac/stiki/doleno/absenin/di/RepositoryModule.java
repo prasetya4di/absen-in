f6572dce1edd51dbeedd07ca8e841c4c8df1f5f8
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-12-21 16:08:44 +0700

    Add user repository to repository module

--- app/src/main/java/id/ac/stiki/doleno/absenin/di/RepositoryModule.java
+++ app/src/main/java/id/ac/stiki/doleno/absenin/di/RepositoryModule.java
@@ -9,25 +9,36 @@ import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ViewModelComponent;
 import id.ac.stiki.doleno.absenin.data.source.local.AbsentDao;
 import id.ac.stiki.doleno.absenin.data.source.local.EventDao;
+import id.ac.stiki.doleno.absenin.data.source.local.UserDao;
+import id.ac.stiki.doleno.absenin.data.source.network.AbsentStore;
+import id.ac.stiki.doleno.absenin.data.source.network.EventStore;
+import id.ac.stiki.doleno.absenin.data.source.network.UserStore;
 import id.ac.stiki.doleno.absenin.repository.AbsentRepository;
 import id.ac.stiki.doleno.absenin.repository.AuthRepository;
 import id.ac.stiki.doleno.absenin.repository.EventRepository;
+import id.ac.stiki.doleno.absenin.repository.UserRepository;
 import id.ac.stiki.doleno.absenin.repository.impl.AbsentRepositoryImpl;
 import id.ac.stiki.doleno.absenin.repository.impl.AuthRepositoryImpl;
 import id.ac.stiki.doleno.absenin.repository.impl.EventRepositoryImpl;
+import id.ac.stiki.doleno.absenin.repository.impl.UserRepositoryImpl;
 
 @Module
 @InstallIn(ViewModelComponent.class)
 public class RepositoryModule {
 
     @Provides
-    public AbsentRepository provideAbsentRepository(AbsentDao absentDao) {
-        return new AbsentRepositoryImpl(absentDao);
+    public AbsentRepository provideAbsentRepository(AbsentDao absentDao, AbsentStore absentStore, UserDao userDao) {
+        return new AbsentRepositoryImpl(absentDao, userDao, absentStore);
     }
 
     @Provides
-    public EventRepository provideEventRepository(EventDao eventDao) {
-        return new EventRepositoryImpl(eventDao);
+    public EventRepository provideEventRepository(EventDao eventDao, EventStore eventStore) {
+        return new EventRepositoryImpl(eventDao, eventStore);
+    }
+
+    @Provides
+    public UserRepository provideUserRepository(UserDao userDao, UserStore userStore) {
+        return new UserRepositoryImpl(userDao, userStore);
     }
 
     @Provides

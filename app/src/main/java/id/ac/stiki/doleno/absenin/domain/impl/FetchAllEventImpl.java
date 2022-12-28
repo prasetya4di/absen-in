d4dc44f11209019cb39fc0531e2fe773d2a3cb89
Pras <prasetyaarfanto@gmail.com>
2022-12-28 11:41:00 +0700

    Add fetch all event use case

---
+++ app/src/main/java/id/ac/stiki/doleno/absenin/domain/impl/FetchAllEventImpl.java
@@ -0,0 +1,29 @@
+package id.ac.stiki.doleno.absenin.domain.impl;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.ac.stiki.doleno.absenin.data.entity.Event;
+import id.ac.stiki.doleno.absenin.domain.FetchAllEvent;
+import id.ac.stiki.doleno.absenin.repository.EventRepository;
+
+public class FetchAllEventImpl implements FetchAllEvent {
+    private final EventRepository eventRepository;
+
+    public FetchAllEventImpl(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public Task<QuerySnapshot> execute() {
+        return eventRepository.get()
+                .addOnSuccessListener(querySnapshots -> {
+                    List<Event> listEvent = new ArrayList<>();
+                    querySnapshots.getDocuments().forEach(document -> listEvent.add(new Event(document.getData())));
+                    eventRepository.create(listEvent);
+                });
+    }
+}

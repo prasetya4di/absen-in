c78c3be6a2afb8bb93ad295839217df6c8f80a2e
Pras <prasetyaarfanto@gmail.com>
2023-01-03 11:47:24 +0700

    Add constant for HERE API

---
+++ app/src/main/java/id/ac/stiki/doleno/absenin/util/constants/HereApiConstant.java
@@ -0,0 +1,6 @@
+package id.ac.stiki.doleno.absenin.util.constants;
+
+public class HereApiConstant {
+    static String BASE_URL = "https://revgeocode.search.hereapi.com";
+    static String API_KEY = "ZN9dlKZ0uz_z6Oom7aq5oDkeP1SeIV9mmD4Z8PKtl2M";
+}

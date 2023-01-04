0eceec779394084d92914d7d3989b9cdf055e5a5
Pras <prasetyaarfanto@gmail.com>
2023-01-04 20:24:26 +0700

    Update my event view model to use new use case

--- app/src/main/java/id/ac/stiki/doleno/absenin/view/admin/ui/my_event/MyEventViewModel.java
+++ app/src/main/java/id/ac/stiki/doleno/absenin/view/admin/ui/my_event/MyEventViewModel.java
@@ -12,29 +12,36 @@ import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
 import id.ac.stiki.doleno.absenin.data.entity.Event;
-import id.ac.stiki.doleno.absenin.domain.FetchAllEvent;
+import id.ac.stiki.doleno.absenin.data.entity.User;
+import id.ac.stiki.doleno.absenin.domain.FetchAllEventByEmail;
 import id.ac.stiki.doleno.absenin.domain.GetAllEvent;
+import id.ac.stiki.doleno.absenin.domain.GetUser;
 
 @HiltViewModel
 public class MyEventViewModel extends ViewModel {
     private final GetAllEvent getAllEvent;
-    private final FetchAllEvent fetchAllEvent;
+    private final GetUser getUser;
+    private final FetchAllEventByEmail fetchAllEvent;
     private final MutableLiveData<MyEventState> _myEventState = new MutableLiveData<>();
     public LiveData<MyEventState> myEventState = _myEventState;
     public LiveData<List<Event>> listMyEvent;
 
     @Inject
-    public MyEventViewModel(GetAllEvent getAllEvent, FetchAllEvent fetchAllEvent) {
+    public MyEventViewModel(GetAllEvent getAllEvent, GetUser getUser, FetchAllEventByEmail fetchAllEvent) {
         this.getAllEvent = getAllEvent;
         this.listMyEvent = getAllEvent.execute();
+        this.getUser = getUser;
         this.fetchAllEvent = fetchAllEvent;
     }
 
     public void fetchMyEvent() {
         _myEventState.postValue(MyEventState.LOADING);
-        AsyncTask.execute(() -> fetchAllEvent.execute()
-                .addOnSuccessListener(success -> _myEventState.postValue(MyEventState.SUCCESS))
-                .addOnFailureListener(failure -> _myEventState.postValue(MyEventState.FAILED)));
+        AsyncTask.execute(() -> {
+            User user = getUser.execute();
+            fetchAllEvent.execute(user.email)
+                    .addOnSuccessListener(success -> _myEventState.postValue(MyEventState.SUCCESS))
+                    .addOnFailureListener(failure -> _myEventState.postValue(MyEventState.FAILED));
+        });
     }
 
     public Boolean isListEventEmpty() {

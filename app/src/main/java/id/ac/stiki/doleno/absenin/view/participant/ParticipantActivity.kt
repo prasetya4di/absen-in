ca36dbe6b0473372989d554da6df1dacc109f289
Pras <prasetyaarfanto@gmail.com>
2023-01-04 10:49:31 +0700

    Update participant activity to show all necessary fragment

--- app/src/main/java/id/ac/stiki/doleno/absenin/view/participant/ParticipantActivity.kt
+++ app/src/main/java/id/ac/stiki/doleno/absenin/view/participant/ParticipantActivity.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import id.ac.stiki.doleno.absenin.R
 import id.ac.stiki.doleno.absenin.databinding.ActivityParticipantBinding
+import id.ac.stiki.doleno.absenin.view.participant.ui.absent.AbsentFragment
+import id.ac.stiki.doleno.absenin.view.participant.ui.all_event.AllEventFragment
+import id.ac.stiki.doleno.absenin.view.participant.ui.history.HistoryFragment
+import id.ac.stiki.doleno.absenin.view.participant.ui.my_event.MyEventFragment
+import id.ac.stiki.doleno.absenin.view.participant.ui.my_profile.MyProfileFragment
 
+@AndroidEntryPoint
 class ParticipantActivity : AppCompatActivity() {
     private lateinit var binding: ActivityParticipantBinding
 
@@ -14,5 +23,28 @@ class ParticipantActivity : AppCompatActivity() {
         binding = ActivityParticipantBinding.inflate(layoutInflater)
         val view: View = binding.root
         setContentView(view)
+
+        val allEventFragment = AllEventFragment()
+        val myEventFragment = MyEventFragment()
+        val absentFragment = AbsentFragment()
+        val historyFragment = HistoryFragment()
+        val myProfileFragment = MyProfileFragment()
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.event_page -> setCurrentPage(allEventFragment)
+                R.id.my_event_page -> setCurrentPage(myEventFragment)
+                R.id.absent_page -> setCurrentPage(absentFragment)
+                R.id.history_page -> setCurrentPage(historyFragment)
+                R.id.profile_page -> setCurrentPage(myProfileFragment)
+            }
+            true
+        }
+
+        setCurrentPage(myEventFragment)
+    }
+
+    private fun setCurrentPage(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.flAdmin, fragment).commit()
     }
 }
